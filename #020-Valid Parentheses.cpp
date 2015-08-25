63cfe83000d2f9409bd80ce13982c6f0a367aeeb
陈明新 <1175326238@qq.com>
2015-08-25 18:15:41 +0800

    leetcode第20题
    leetcode第20题

---
+++ #020-Valid Parentheses.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+/*这是一道关于括号匹配的题，基本思路其实就是通过栈的思想来做，这里通过vector来实现栈的功能，一个个读取括号字符串中的字符，
+然后在压入栈中判断即将加入栈中的字符与栈首的字符串是否是括号匹配，如果匹配则将栈首的字符退出栈，继续读取字符进行判断操作。
+最后栈为空则说明括号全部匹配成功，返回1，否则返回0。
+*/
+/*************************在leetcode上直接提交以下代码即可******************************/
+class Solution 
+{
+public:
+    bool isValid(string s) 
+    {
+        vector<char> vchar;
+        int len = s.length();
+        for(int i = 0; i < len; i++)
+        {
+        	int size = vchar.size();
+       		if(size != 0)
+       		{
+       			if(isCorrectorder(vchar[size - 1], s[i]))
+       				vchar.pop_back();
+       			else
+       				vchar.push_back(s[i]);
+       		}
+       		else
+       			vchar.push_back(s[i]);
+        }
+
+        if(vchar.size() == 0)
+        	return 1;
+        else
+        	return 0;
+    }
+
+    bool isCorrectorder(char a, char b)
+    {
+    	if((a== '(' && b == ')') || (a == '{' && b == '}') || (a == '[' && b == ']'))
+    		return 1;
+    	return 0;
+    }
+};
+/*************************在leetcode上直接提交以上代码即可******************************/
+int main()
+{
+	string s;
+	cin >> s;
+	Solution solu;
+	bool bo = solu.isValid(s);
+	cout << bo << endl;
+}
